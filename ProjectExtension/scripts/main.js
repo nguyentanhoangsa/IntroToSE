9c132a44bdebc598356e7f96d8e31e43f2c7ede3
nths <nguyentanhoangsa@gmail.com>
2023-12-17 00:38:29 +0700

    Thêm các lựa chọn select ngôn ngữ cho chức năng dịch + Tái cấu trúc và comment code lại file main.js để dễ hiểu và bảo trì

--- ProjectExtension/scripts/main.js
+++ ProjectExtension/scripts/main.js
@@ -1,55 +1,5 @@
-// Tạo hàm để thiết lập URL của hình ảnh
-function setIconUrls() {
-    const iconUrls = [
-      "icon-32.png",
-      "icon-x.png",
-      "icon-translate.png",
-      "icon-revise.png",
-      "icon-summarize.png",
-      "icon-send.png",
-    ];
-  
-    const iconUrlsMap = {};
-  
-    iconUrls.forEach((icon) => {
-      const iconName = icon.replace(/\.png$/, ""); // Loại bỏ phần mở rộng của tên file
-      iconUrlsMap[iconName] = chrome.runtime.getURL(`img/${icon}`);
-    });
-  
-    return iconUrlsMap;
-  }
-  
-  // Hàm để thiết lập nội dung cho popup
-  function setPopupContent(data) {
-    const popup = document.getElementById("file-container");
-    popup.innerHTML = data;
-  
-    const icons = setIconUrls();
-  
-    document.getElementById("icon").src = icons["icon-32"];
-    document.getElementById("icon-x").src = icons["icon-x"];
-    document.getElementById("icon-translate").src = icons["icon-translate"];
-    document.getElementById("icon-revise").src = icons["icon-revise"];
-    document.getElementById("icon-summarize").src = icons["icon-summarize"];
-    document.getElementById("icon-send").src = icons["icon-send"];
-  }
-  
-  function createPopup() {
-    document.addEventListener("click", () => {
-      console.log("Batdau");
-      fetch(chrome.runtime.getURL("popup_func/popup_func.html"))
-        .then((response) => response.text())
-        .then((data) => {
-          setPopupContent(data);
-        })
-        .catch((error) => {
-          console.error("Failed to fetch popup HTML:", error);
-        });
-  
-      console.log("aaaaaaaaaaaaaaaaa");
-    });
-  }
-  
+//==========================Bắt sự kiện khi nào thì làm hoạt động action hiển thị extension======================  
+  //Bắt sự kiện Click hoặc là keyup để thực hiện action và hiển thị nút extension
   document.addEventListener("click", action, false);
   
   document.addEventListener(
@@ -59,42 +9,44 @@ function setIconUrls() {
     },
     false
   );
-  
-  function createIcon() {
-    var icon = document.createElement("button");
-    icon.className = "icon"; // Add classes or styles for your icon
-    icon.style.width = "20px";
-    icon.style.height = "20px";
-    //icon.value = "6"
-  
-    // Thêm sự kiện click cho icon để hiển thị popup
-    icon.addEventListener("click", function () {
-      toggleFileVisibility(); // Gọi hàm hiển thị popup khi icon được click
-    });
-    return icon;
+
+ //================================================================ 
+
+
+//==========================Kiểm tra điều kiện để hiển thị icon và thực hiện add Icon======================
+     // Kiểm tra đầu vào là input hay là textarea 
+   function checkInput(clickedElement) {
+    return (
+      (clickedElement.tagName == "INPUT" &&
+        (clickedElement.type == "text" || clickedElement.type == "search")) ||
+      clickedElement.tagName == "TEXTAREA" ||
+      clickedElement.role == "textbox"
+    );
   }
+
+  // Bắt sự kiện lắng nghe ở trên để hiện thì 
+  function action() {
+    var clickedElement = document.activeElement;
+    // console.log("Hello");
   
-  function createContainer() {
-    var container = document.createElement("div");
-    container.id = "file-container"; // Add classes or styles for your icon
-    container.style.display = "none";
-    return container;
+    // Kiểm tra đầu vào để nếu đúng thì mới addIcon vào cái vùng Clicked được, còn nếu không thì tìm vùng chỗ
+    // có trong bodey mà remove
+    if (checkInput(clickedElement)) {
+      addIconToClickedField(clickedElement);
+    } else {
+      // Remove the icon if clicked elsewhere on the document
+      var icon = document.querySelector(".icon");
+      if (icon && icon == clickedElement.nextSibling) {
+        icon.parentNode.removeChild(icon);
+      }
+    }
   }
-  
-  function createImg() {
-    var img = document.createElement("img");
-    img.src = "https://cdn-icons-png.flaticon.com/512/6913/6913644.png";
-    img.alt = "...";
-    img.style.height = "18px";
-    img.style.width = "18px";
-    img.style.position = "absolute";
-    img.style.top = "0";
-    img.style.left = "0";
-    return img;
-  }
-  
-  //Function to add icon to the clicked input or textarea field
-  function addIconToClickedField(clickedElement) {
+   //================================================================ 
+
+
+ //==========================Các hàm add Icon====================== 
+   //Hàm add icon
+   function addIconToClickedField(clickedElement) {
     var icon = createIcon();
     var container = createContainer();
   
@@ -103,12 +55,12 @@ function setIconUrls() {
     const compStyles = getComputedStyle(clickedElement);
     const parentStyles = getComputedStyle(clickedElement.parentNode);
   
-    // Remove existing icon if present
+    // Điều kiện if này là nếu có 2+ ô input trở lên thì xóa icon ở ô input cũ và hiển thị ở ô mới
     if (existingIcon) {
       existingIcon.parentNode.removeChild(existingIcon);
     }
   
-    // Add icon to the clicked input or textarea field
+    // set position
     icon.style.position = "absolute";
     icon.style.top =
       (
@@ -135,46 +87,107 @@ function setIconUrls() {
     var img = createImg();
     icon.appendChild(img);
   }
-  
-  function checkInput(clickedElement) {
-    return (
-      (clickedElement.tagName == "INPUT" &&
-        (clickedElement.type == "text" || clickedElement.type == "search")) ||
-      clickedElement.tagName == "TEXTAREA" ||
-      clickedElement.role == "textbox"
-    );
+  //Hàm tạo Container bao bọc bên ngoài cái Image số 6
+  function createContainer() {
+    var container = document.createElement("div");
+    container.id = "file-container"; // Add classes or styles for your icon
+    container.style.display = "none";
+    return container;
   }
-  
-  // Event listener for clicking on input or textarea fields
-  function action() {
-    var clickedElement = document.activeElement;
-    // console.log("Hello");
-  
-    // Check if the clicked element is an input or textarea field or the icon itself
-    if (checkInput(clickedElement)) {
-      addIconToClickedField(clickedElement);
-    } else {
-      // Remove the icon if clicked elsewhere on the document
-      var icon = document.querySelector(".icon");
-      if (icon && icon == clickedElement.nextSibling) {
-        icon.parentNode.removeChild(icon);
-      }
-    }
+  //Hàm khởi tạo hình ảnh số 6-> Team 6 để chứa trong icon
+  function createImg() {
+    var img = document.createElement("img");
+    img.src = "https://cdn-icons-png.flaticon.com/512/6913/6913644.png";
+    img.alt = "...";
+    img.style.height = "18px";
+    img.style.width = "18px";
+    img.style.position = "absolute";
+    img.style.top = "0";
+    img.style.left = "0";
+    return img;
   }
+  //Hàm khởi tạo nút button bên ngoài để chứa số 6 tạo ra từ hàm createImg ở trên
+  function createIcon() {
+    var icon = document.createElement("button");
+    icon.className = "icon"; // Add classes or styles for your icon
+    icon.style.width = "20px";
+    icon.style.height = "20px";
   
-  var isFileVisible = false;
-  function toggleFileVisibility() {
-    createPopup();
-    const fileContainer = document.getElementById("file-container");
-    console.log(fileContainer);
-    if (!isFileVisible) {
-      // Nếu file không hiển thị, hiển thị nó
-      fileContainer.style.display = "block";
-      isFileVisible = true;
-    } else {
-      // Nếu file đang hiển thị, ẩn nó
-      fileContainer.style.display = "none";
-      isFileVisible = false;
-    }
+    // Thêm sự kiện click cho icon để hiển thị popup
+    icon.addEventListener("click", function () {
+      toggleFileVisibility(); // Gọi hàm hiển thị popup khi icon được click
+    });
+    return icon;
   }
+ //================================================================ 
+
+
+//==================Các hàm thực hiện khi click vào icon thì hiển thị ra bảng để tương tác vs user===========
+//Hàm hiển thị chính của toggle hiển thị
+var isFileVisible = false;
+function toggleFileVisibility() {
+  createPopup();
+  const fileContainer = document.getElementById("file-container");
+  console.log(fileContainer);
+  if (!isFileVisible) {
+    // Nếu file không hiển thị, hiển thị nó
+    fileContainer.style.display = "block";
+    isFileVisible = true;
+  } else {
+    // Nếu file đang hiển thị, ẩn nó
+    fileContainer.style.display = "none";
+    isFileVisible = false;
+  }
+}
+// Hàm để thiết lập nội dung cho popup
+function createPopup() {
+  document.addEventListener("click", () => {
+    console.log("Batdau");
+    fetch(chrome.runtime.getURL("popup_func/popup_func.html"))
+      .then((response) => response.text())
+      .then((data) => {
+        setPopupContent(data);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch popup HTML:", error);
+      });
+
+    console.log("aaaaaaaaaaaaaaaaa");
+  });
+}
+function setPopupContent(data) {
+  const popup = document.getElementById("file-container");
+  popup.innerHTML = data;
+
+  const icons = setIconUrls();
+
+  document.getElementById("icon").src = icons["icon-32"];
+  document.getElementById("icon-x").src = icons["icon-x"];
+  document.getElementById("icon-translate").src = icons["icon-translate"];
+  document.getElementById("icon-revise").src = icons["icon-revise"];
+  document.getElementById("icon-summarize").src = icons["icon-summarize"];
+  document.getElementById("icon-send").src = icons["icon-send"];
+}
+// Tạo hàm để thiết lập URL của hình ảnh
+function setIconUrls() {
+    const iconUrls = [
+      "icon-32.png",
+      "icon-x.png",
+      "icon-translate.png",
+      "icon-revise.png",
+      "icon-summarize.png",
+      "icon-send.png",
+    ];
+  
+    const iconUrlsMap = {};
+  
+    iconUrls.forEach((icon) => {
+      const iconName = icon.replace(/\.png$/, ""); // Loại bỏ phần mở rộng của tên file
+      iconUrlsMap[iconName] = chrome.runtime.getURL(`img/${icon}`);
+    });
+  
+    return iconUrlsMap;
+  }
+
+
   
