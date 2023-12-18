54d5d581a317d236d1b819ccf7196ea58593135b
nths <nguyentanhoangsa@gmail.com>
2023-12-19 00:44:18 +0700

    Xóa head của popup_func.html và thiết lập kích thước cho file-container, thiết lập vị trí cơ sở cho file-container

--- ProjectExtension/scripts/main.js
+++ ProjectExtension/scripts/main.js
@@ -33,12 +33,6 @@ function action() {
     // có trong bodey mà remove
     if (checkInput(clickedElement)) {
         addIconToClickedField(clickedElement);
-    } else {
-        // Remove the icon if clicked elsewhere on the document
-        var icon = document.querySelector(".icon");
-        if (icon && icon == clickedElement.nextSibling) {
-            icon.parentNode.removeChild(icon);
-        }
     }
 }
 //================================================================ 
@@ -48,16 +42,18 @@ function action() {
 //Hàm add icon
 function addIconToClickedField(clickedElement) {
     var icon = createIcon();
-    var container = createContainer();
 
     var existingIcon = document.querySelector(".icon");
+    var divContainer = document.getElementById("file-container");
     clickedElement.parentNode.style.position = "relative";
     const compStyles = getComputedStyle(clickedElement);
     const parentStyles = getComputedStyle(clickedElement.parentNode);
 
     // Điều kiện if này là nếu có 2+ ô input trở lên thì xóa icon ở ô input cũ và hiển thị ở ô mới
+    //xóa luôn cả file-container
     if (existingIcon) {
         existingIcon.parentNode.removeChild(existingIcon);
+        divContainer.parentNode.removeChild(divContainer);
     }
 
     // set position
@@ -74,19 +70,29 @@ function addIconToClickedField(clickedElement) {
         }px`;
 
     clickedElement.parentNode.appendChild(icon);
-    clickedElement.parentNode.insertBefore(container, clickedElement.nextSibling);
+
+    var container = createContainer();
+    document.body.appendChild(container);
+    //clickedElement.parentNode.insertBefore(container, clickedElement.nextSibling);
 
     // createPopup();
     // openCustomPopup();
 
     var img = createImg();
     icon.appendChild(img);
+
+    // Thêm sự kiện click cho icon để hiển thị popup
+    icon.addEventListener("click", function () {
+        toggleFileVisibility(clickedElement); // Gọi hàm hiển thị popup khi icon được click
+    });
 }
 //Hàm tạo Container de bao bọc bên ngoài popup_func chon chuc nang
 function createContainer() {
     var container = document.createElement("div");
     container.id = "file-container"; // Add classes or styles for your icon
-    container.style.display = "none";
+    // container.style.display = "block";
+    container.style.position = "fixed"
+    container.style.zIndex = "777777";
     return container;
 }
 //Hàm khởi tạo hình ảnh số 6-> Team 6 để chứa trong icon
@@ -107,11 +113,7 @@ function createIcon() {
     icon.className = "icon"; // Add classes or styles for your icon
     icon.style.width = "20px";
     icon.style.height = "20px";
-
-    // Thêm sự kiện click cho icon để hiển thị popup
-    icon.addEventListener("click", function () {
-        toggleFileVisibility(); // Gọi hàm hiển thị popup khi icon được click
-    });
+    icon.style.zIndex = "666666";
     return icon;
 }
 //================================================================ 
@@ -120,39 +122,43 @@ function createIcon() {
 //==================Các hàm thực hiện khi click vào icon thì hiển thị ra bảng để tương tác vs user===========
 //Hàm hiển thị chính của toggle hiển thị
 var isFileVisible = false;
-function toggleFileVisibility() {
-    createPopup();
+function toggleFileVisibility(clickedElement) {
+    createPopup(clickedElement);
     const fileContainer = document.getElementById("file-container");
-    console.log(fileContainer);
-    if (!isFileVisible) {
-        // Nếu file không hiển thị, hiển thị nó
-        fileContainer.style.display = "block";
-        isFileVisible = true;
-    } else {
-        // Nếu file đang hiển thị, ẩn nó
-        fileContainer.style.display = "none";
-        isFileVisible = false;
-    }
+    //console.log(fileContainer);
 }
 // Hàm để thiết lập nội dung cho popup
-function createPopup() {
-    document.addEventListener("click", () => {
-        console.log("Batdau");
-        fetch(chrome.runtime.getURL("popup_func/popup_func.html"))
-            .then((response) => response.text())
-            .then((data) => {
-                setPopupContent(data);
-            })
-            .catch((error) => {
-                console.error("Failed to fetch popup HTML:", error);
-            });
+function createPopup(clickedElement) {
+    //console.log("Batdau");
+    fetch(chrome.runtime.getURL("popup_func/popup_func.html"))
+        .then((response) => response.text())
+        .then((data) => {
+            setPopupContent(data, clickedElement);
+        })
+        .catch((error) => {
+            console.error("Failed to fetch popup HTML:", error);
+        });
 
-        console.log("aaaaaaaaaaaaaaaaa");
-    });
+    //console.log("aaaaaaaaaaaaaaaaa");
 }
-function setPopupContent(data) {
-    const popup = document.getElementById("file-container");
-    popup.innerHTML = data;
+function setPopupContent(data, clickedElement) {
+    const popup_func = document.getElementById("file-container");
+    //Khúc chèn nội dung vào container
+    popup_func.innerHTML = data;
+    // Gán kích thước của div bằng kích thước tối đa của nội dung bên trong
+    const popupFunc = popup_func.querySelector("#popup-func");
+    popupFunc.style.position = "absolute";
+    // Gán chiều rộng thẻ container bằng thèn con lớn nhất
+    const rect = popupFunc.getBoundingClientRect();
+    popup_func.style.width = `${rect.width}px`;
+    popup_func.style.height = `${rect.height}px`;
+
+    //Set vị trí cho thẻ container bao ngoài
+    const parentClickElementRect = clickedElement.parentNode.getBoundingClientRect();
+    const top = parentClickElementRect.top + parentClickElementRect.height;
+    const left = parentClickElementRect.left + parentClickElementRect.width;
+    popup_func.style.top = `${top}px`;
+    popup_func.style.left = `${left}px`;
 
     const icons = setIconUrls();
 
